8888382c866feb16be18896eb1a24372051a8f31
Yoshi Hollez <yoshi.hollez@student.vives.be>
2018-04-16 17:49:52 +0200

    v3

--- biometric-station-arduino/biometric-station-arduino-yoshi/biometric-station-arduino-yoshi.ino
+++ biometric-station-arduino/biometric-station-arduino-yoshi/biometric-station-arduino-yoshi.ino
@@ -5,6 +5,7 @@
 int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
 int THRESHOLD = 550;            // Determine which Signal to "count as a beat", and which to ingore.
+int MAXTHRESHOLD = 1500;
 float temperature;
 TMP102 sensor0(0x48);
 MMA8452Q accel;
@@ -18,47 +19,46 @@ void setup(){
 }
 
 void loop(){
-    //Serial.print("[");
-    accelloSensor();
-    //Serial.print("|");
-    heartBeat();
-    //Serial.print("|");
-    temperatureSensor();
-    //Serial.print("]");    
+    Serial.print("[");
+    Serial.print(accelloSensor());
+    Serial.print("|");
+    Serial.print(heartBeat());
+    Serial.print("|");
+    Serial.print(temperatureSensor());
+    Serial.print("]");    
     Serial.println();
     delay(10);
 }
-void temperatureSensor(){
+String temperatureSensor(){
   sensor0.wakeup();
   temperature = sensor0.readTempC();
   sensor0.sleep();
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
+
+    String temperatureString="";
+    temperatureString = String(temperature,4);
+    return temperatureString;
 }
-void accelloSensor(){
-    if (accel.available()){
+String accelloSensor(){
+    accel.available();
     accel.read();
-    printCalculatedAccels();
-    }
+    
+    String xString="";
+    String yString="";
+    String zString="";
+    xString = String(accel.cx,4);
+    yString = String(accel.cy,4);
+    zString = String(accel.cz,4);
+        
+    return xString + "," + yString + "," + zString;
 }
-void heartBeat(){
+
+String heartBeat(){
   Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value.
                                               // Assign this value to the "Signal" variable.
     if(Signal > THRESHOLD){
-       Serial.println(Signal);    // Send the Signal value to Serial Plotter.
-       return Signal;
-    }
-    return 0;
-}
-void printCalculatedAccels()
-{ 
-  Serial.print("x: ");
-  Serial.print(accel.cx, 3);
-  Serial.print("\t");
-  Serial.print("y: ");
-  Serial.print(accel.cy, 3);
-  Serial.print("\t");
-  Serial.print("z: ");
-  Serial.print(accel.cz, 3);
-  Serial.print("\t");
+      //Serial.println(Signal);    // Send the Signal value to Serial Plotter.
+      String signalString = String(Signal);
+      return signalString;
+      }
+      return "0";
 }
